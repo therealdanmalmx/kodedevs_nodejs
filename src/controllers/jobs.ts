7d660ee6dacc9353a0101c1057745883511c4556
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2023-05-20 22:42:03 +0200

    Add delete function

--- src/controllers/jobs.ts
+++ src/controllers/jobs.ts
@@ -1,31 +1,58 @@
 import { db } from '../utils/db.server';
 
-export const createJob = async (req: any, res: any) => {
-    const job = await db.jobs.create({
-        data: { ...req.body}
+export const createJob = async (job: any) => {
+    const { title, description, qualification, responsibilities, location, company_name, company_website, company_tagline, company_logo } = job;
+    return await db.jobs.create({
+        data: {
+            title,
+            description,
+            qualification,
+            responsibilities,
+            location,
+            company_name,
+            company_website,
+            company_tagline,
+            company_logo,
+        },
     });
-    return res.json(job);
 }
 
-export const getAllJobs = async (req: any, res: any) => {
-    const jobs = await db.jobs.findMany({});
-    return res.json(jobs);
+export const updateJob = async (id: string, job: any ) => {
+    const { title, description, qualification, responsibilities, location, company_name, company_website, company_tagline, company_logo } = job;
+    return await db.jobs.update({
+        where: {
+            id,
+        },
+        data: {
+            title,
+            description,
+            qualification,
+            responsibilities,
+            location,
+            company_name,
+            company_website,
+            company_tagline,
+            company_logo,
+        },
+    });
 }
 
-export const getJob = async (req: any, res: any, id: string ) => {
-    const job = await db.jobs.findUnique({
+export const getAllJobs = async () => {
+    return await db.jobs.findMany({});
+}
+
+export const getJob = async (id: string ) => {
+    return await db.jobs.findUnique({
         where: {
             id,
         },
     });
-    return res.json(job);
 }
 
-export const deleteJob = async (req: any, res: any, id: string ) => {
-    const job = await db.jobs.delete({
+export const deleteJob = async (id: string ) => {
+    return await db.jobs.delete({
         where: {
             id,
         },
     });
-    return res.json(job);
 }
--- src/routes/jobs.ts
+++ src/routes/jobs.ts
@@ -8,12 +8,29 @@ import * as Jobs from '../controllers/jobs';
 export const jobsRouter = express.Router();
 
 
-jobsRouter.post('/',  async (req: Request, res: Response) => {
+jobsRouter.post('/',
+    body("title").isString(),
+    body("description").isString(),
+    body("qualification").isString(),
+    body("responsibilities").isString(),
+    body("location").isString(),
+    body("company_name").isString(),
+    body("company_website").isString(),
+    body("company_tagline").isString(),
+    body("company_logo").isString(),
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res
+                .status(400)
+                .json({ errors: errors.array() });
+        }
     try {
-        const job = await Jobs.createJob(req, res);
+        const job = req.body;
+        const newJob = await Jobs.createJob(job);
         return res
             .status(201)
-            .json(job);
+            .json(newJob);
 
     } catch (error: any) {
         return res
@@ -23,10 +40,25 @@ jobsRouter.post('/',  async (req: Request, res: Response) => {
 
 });
 
+jobsRouter.put('/:id', async (req: Request, res: Response) => {
+    const id = req.params.id;
+    try {
+        const job = req.body;
+        const updatedJob = await Jobs.updateJob(id, job);
+        return res
+            .status(200)
+            .json(updatedJob);
+
+    } catch (error: any) {
+        return res
+            .status(500)
+            .json({ message: error.message });
+    }
+});
 
 jobsRouter.get('/', async (req: Request, res: Response) => {
     try {
-        const jobs = await Jobs.getAllJobs(req, res);
+        const jobs = await Jobs.getAllJobs();
         return res
             .status(200)
             .json(jobs);
@@ -42,7 +74,7 @@ jobsRouter.get('/', async (req: Request, res: Response) => {
 jobsRouter.get('/:id', async (req: Request, res: Response) => {
     const id = req.params.id;
     try {
-        const job: any = await Jobs.getJob(req, res, id);
+        const job: any = await Jobs.getJob(id);
         if (job) {
             return res
                 .status(200)
@@ -59,12 +91,13 @@ jobsRouter.get('/:id', async (req: Request, res: Response) => {
 jobsRouter.delete('/:id', async (req: Request, res: Response) => {
     const id = req.params.id;
     try {
-        const job = await Jobs.deleteJob(req, res, id);
         if (!id) {
             return res
-                .status(404)
-                .json({ message: `Job with id ${id} was not found` });
+            .status(404)
+            .json({ message: `Job with id ${id} was not found` });
         }
+
+        const job = await Jobs.deleteJob(id);
         return res.status(200).json(job);
 
     } catch (error) {
